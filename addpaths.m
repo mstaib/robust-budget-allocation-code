3f5d5537bb2f094e4efc6dbcb285fa5d9a713531
Matthew Staib <matt@mstaib.mit.edu>
2017-06-08 22:10:04 -0400

    add new folder to path

--- addpaths.m
+++ addpaths.m
@@ -1,4 +1,5 @@
 addpath(genpath(fullfile(pwd)))
 addpath(genpath(fullfile(pwd,'continuous_submodular_minimization')))
+addpath(genpath(fullfile(pwd,'robust_budget_allocation')))
 addpath(genpath(fullfile(pwd,'experiments')))
 addpath(genpath(fullfile(pwd,'test_suite')))
